9d6f6c7c813565e2129ca11d09236d7a895d90df
tganyani <tatendaganyani5@gmail.com>
2025-08-14 14:43:33 +0300

    deleted course create

--- components/user/userList.tsx
+++ components/user/userList.tsx
@@ -1,37 +1,51 @@
-import { List, Datagrid, TextField, DateField ,TextInput,SelectInput, SimpleList,} from 'react-admin';
+import {
+  List,
+  Datagrid,
+  TextField,
+  DateField,
+  TextInput,
+  SelectInput,
+  SimpleList,
+} from "react-admin";
 
 import { useMediaQuery } from "@mui/material";
 
-
-export const UserList = (props:any) => {
+export const UserList = (props: any) => {
   const isSmall = useMediaQuery("(max-width:600px)");
   const isbig = useMediaQuery("(max-width:1000px)");
-  const choices = [{id:1,name:"student"},{id:2,name:"tutor"},{id:3,name:"admin"}]
-  const postFilters = [
-    <TextInput label="search by first name" source="firstName"  alwaysOn />,
-    <TextInput label="search by email" source="email" />,
-    <SelectInput source="Role" choices={choices} optionValue="name" />
+  const choices = [
+    { id: 1, name: "student" },
+    { id: 2, name: "tutor" },
+    { id: 3, name: "admin" },
   ];
-  
+  const postFilters = [
+    <TextInput
+      key={1}
+      label="search by first name"
+      source="firstName"
+      alwaysOn
+    />,
+    <TextInput key={2} label="search by email" source="email" />,
+    <SelectInput key={3} source="Role" choices={choices} optionValue="name" />,
+  ];
+
   return (
     <List {...props} filters={postFilters}>
       {isSmall ? (
-              <SimpleList
-                primaryText={(record) => `${record.firstName}   ${record.lastName}`}
-                secondaryText={(record) => record.email}
-                tertiaryText={(record) => record.Role}
-              />
-            ) : (
-              <Datagrid>
-              <TextField source="firstName" />
-              <TextField source="lastName" />
-              <TextField source="email" />
-              <TextField source="Role" />
-              {!isbig&&<DateField source="createdAt" />}
-            </Datagrid>
-            )}
-     
+        <SimpleList
+          primaryText={(record) => `${record.firstName}   ${record.lastName}`}
+          secondaryText={(record) => record.email}
+          tertiaryText={(record) => record.Role}
+        />
+      ) : (
+        <Datagrid>
+          <TextField source="firstName" />
+          <TextField source="lastName" />
+          <TextField source="email" />
+          <TextField source="Role" />
+          {!isbig && <DateField source="createdAt" />}
+        </Datagrid>
+      )}
     </List>
-  )
-
-}
+  );
+};
